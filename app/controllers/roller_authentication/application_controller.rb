d77f73657007d9f4a57ee667bf535bc897cf93db
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-04 15:04:58 -0700

    Start ApplicationHelper
    Meant to be included in app's ApplicationController

--- app/controllers/roller_authentication/application_controller.rb
+++ app/controllers/roller_authentication/application_controller.rb
@@ -1,5 +1,7 @@
 module RollerAuthentication
   class ApplicationController < ActionController::Base
+    include ApplicationHelper
+
     protect_from_forgery with: :exception
   end
 end
--- app/helpers/roller_authentication/application_helper.rb
+++ app/helpers/roller_authentication/application_helper.rb
@@ -1,4 +1,39 @@
 module RollerAuthentication
   module ApplicationHelper
+    def current_user=(user)
+      @current_user = user
+    end
+
+    def current_user
+      @current_user
+    end
+
+    def authenticate
+      authenticator = DummyAuthenticator.new(
+        users: User.joins(:employee),
+        session: session
+      )
+
+      @current_user = authenticator.authenticate
+      redirect_to roller_authentication.login_url unless current_user
+    end
+
+    def authenticate_email(email)
+      authenticator = DummyAuthenticator.new(
+        users: User.joins(:employee),
+        session: session
+      )
+
+      authenticator.authenticate_email(email)
+    end
+
+    def deauthenticate
+      authenticator = DummyAuthenticator.new(
+        users: User.joins(:employee),
+        session: session
+      )
+
+      authenticator.deauthenticate
+    end
   end
 end
