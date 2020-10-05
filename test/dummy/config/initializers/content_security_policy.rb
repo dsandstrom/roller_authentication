4323afd23b588e43dc8ce01519e1855d346ec242
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-05 16:23:53 -0700

    Fix rubocop errors in test/dummy

--- test/dummy/config/initializers/content_security_policy.rb
+++ test/dummy/config/initializers/content_security_policy.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-# Define an application-wide content security policy
-# For further information see the following documentation
+# Define an application-wide content security policy For further information see
+# the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 # Rails.application.config.content_security_policy do |policy|
@@ -18,10 +18,12 @@
 # end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+# Rails.application.config.content_security_policy_nonce_generator
+#   = -> request { SecureRandom.base64(16) }
 
 # Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+# Rails.application.config.content_security_policy_nonce_directives =
+#   %w(script-src)
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
--- test/roller_authentication_test.rb
+++ test/roller_authentication_test.rb
@@ -2,12 +2,14 @@
 
 require 'test_helper'
 
-class RollerAuthentication::Test < ActiveSupport::TestCase
-  test 'is a module' do
-    assert_kind_of Module, RollerAuthentication
-  end
+module RollerAuthentication
+  class Test < ActiveSupport::TestCase
+    test 'is a module' do
+      assert_kind_of Module, RollerAuthentication
+    end
 
-  test 'has configs' do
-    assert_equal Rails.application.config, RollerAuthentication.config
+    test 'has configs' do
+      assert_equal Rails.application.config, RollerAuthentication.config
+    end
   end
 end
