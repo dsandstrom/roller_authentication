f54224974e757be47be880903184fc29f89f0d7e
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-04 18:52:15 -0700

    Start module tests

--- test/roller_authentication_test.rb
+++ test/roller_authentication_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class RollerAuthentication::Test < ActiveSupport::TestCase
-  test "truth" do
+  test "is a module" do
     assert_kind_of Module, RollerAuthentication
+  end
+
+  test "has configs" do
+    assert_equal Rails.application.config, RollerAuthentication.config
   end
 end
